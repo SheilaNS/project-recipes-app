583e662ac5041dd40747498d7785b3888df484df
SheilaNS <shei.nsantos@gmail.com>
2022-04-26 20:36:13 -0300

    feat: requisitos 11 e 12 ok
    Co-authored-by: Laís Nametala Silva <lalanametala@users.noreply.github.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, searchOn }) {
+  const history = useHistory();
+  const [renderSearch, setRenderSearch] = useState(false);
+
+  const handleProfileClick = () => history.push('/profile');
+  const handleSearchClick = () => setRenderSearch(!renderSearch);
+
   return (
     <header>
       <button
         type="button"
         data-testid="profile-top-btn"
         src={ profileIcon }
+        onClick={ handleProfileClick }
       >
         <img
           src={ profileIcon }
@@ -17,11 +25,14 @@ function Header({ title, searchOn }) {
         />
       </button>
       <h1 data-testid="page-title">{ title }</h1>
+      { renderSearch && (
+        <input data-testid="search-input" />)}
       { searchOn && (
         <button
           type="button"
           data-testid="search-top-btn"
           src={ searchIcon }
+          onClick={ handleSearchClick }
         >
           <img
             src={ searchIcon }
