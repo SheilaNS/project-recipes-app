e191c332977c0ff9e710043403e6819b9856e4b2
Lalá Nametala <lalanametala@gmail.com>
2022-04-26 18:15:21 -0300

    Requisito 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = ({ target: { value, type } }) => {
+    setUserInfo({ ...userInfo, [type]: value });
+  };
+
+  const formValidation = () => {
+    const minPassLength = 6;
+    const validEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    const { email, password } = userInfo;
+
+    return password.length > minPassLength && email.match(validEmail);
+  };
+
   return (
-    <>
+    <form>
       <input
         type="email"
         data-testid="email-input"
+        value={ userInfo.email }
+        onChange={ handleChange }
       />
       <input
         type="password"
         data-testid="password-input"
+        value={ userInfo.password }
+        onChange={ handleChange }
       />
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ !formValidation() }
       >
         Enter
       </button>
-    </>
+    </form>
   );
 }
 
