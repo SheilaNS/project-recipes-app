b8927bc936f495e22f375feca35fb563258129b7
Lalá Nametala <lalanametala@gmail.com>
2022-04-26 17:57:55 -0300

    requisitos 2, 3 e 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 function Login() {
   return (
-    <h1>
-      Login
-    </h1>
+    <>
+      <input
+        type="email"
+        data-testid="email-input"
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </>
   );
 }
 
