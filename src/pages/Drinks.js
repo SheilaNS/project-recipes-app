732bb83f40d33a4f1a70ef26d8bb093e326a6581
Lalá Nametala <lalanametala@gmail.com>
2022-04-27 17:22:17 -0300

    [FEAT] Requisito 17 adaptado ao teste

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import Header from '../components/Header';
+import React, { useContext } from 'react';
+import Cards from '../components/Cards';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
+import AppContext from '../context/AppContext';
 
 function Drinks() {
+  const { drinks } = useContext(AppContext);
+
   return (
     <>
       <Header title="Drinks" searchOn />
       <Footer />
+      {drinks.length > 1 && (
+        drinks.map(({ strDrink, strDrinkThumb }, index) => {
+          const maxCards = 12;
+          if (index < maxCards) {
+            return (<Cards
+              recipe={ { recipeName: strDrink, recipeImg: strDrinkThumb, index } }
+              key={ index }
+            />);
+          }
+          return null;
+        })
+      )}
     </>
   );
 }
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Cards from '../components/Cards';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import AppContext from '../context/AppContext';
 
 function Foods() {
+  const { meals } = useContext(AppContext);
+
   return (
     <>
       <Header title="Foods" searchOn />
       <Footer />
+      {meals.length > 1 && (
+        meals.map(({ strMeal, strMealThumb }, index) => {
+          const maxCards = 10;
+          if (index < maxCards) {
+            return (<Cards
+              recipe={ { recipeName: strMeal, recipeImg: strMealThumb, index } }
+              key={ index }
+            />);
+          }
+          return null;
+        })
+      )}
     </>
   );
 }
