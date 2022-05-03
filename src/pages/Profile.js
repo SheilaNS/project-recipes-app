205a86613d70ff9c78d1250d7613a772c6e94d02
Lalá Nametala <lalanametala@gmail.com>
2022-05-03 14:27:26 -0300

    Criaçao de Branch

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function Profile() {
   return (
     <>
       <Header title="Profile" searchOn={ false } />
+      a
       <Footer />
     </>
   );
