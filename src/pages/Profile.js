53a6506a47e10820c4f3e4fc402460cc7f09ca3e
Thamires Lourenço <thami.lourenco@outlook.com>
2022-05-03 16:04:05 -0300

    [FEAT] tela de perfil/ requisitos 83 a 87

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,35 +1,56 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
-  /* const { email } = JSON.parse(localStorage.getItem('userInfo.email')); */
+  const email = localStorage.getItem('user');
+
+  const history = useHistory();
+
+  const handleClickFavorite = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const handleClickDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
+
+  const handleClickLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <>
       <Header title="Profile" searchOn={ false } />
-      a
-      <Footer />
       <main>
-        <p data-testid="profile-email">email</p>
+        <p data-testid="profile-email">{email}</p>
         <button
           data-testid="profile-done-btn"
           type="button"
+          onClick={ handleClickDoneRecipes }
         >
           Done Recipes
         </button>
+
         <button
           data-testid="profile-favorite-btn"
           type="button"
+          onClick={ handleClickFavorite }
         >
           Favorite Recipes
         </button>
+
         <button
           data-testid="profile-logout-btn"
           type="button"
+          onClick={ handleClickLogout }
         >
           Logout
         </button>
       </main>
+      <Footer />
     </>
   );
 }
