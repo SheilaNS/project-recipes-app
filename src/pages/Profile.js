3d229cf00b5cda6c6894f5d4aa85203c7ba5b2d6
Thamires Lourenço <thami.lourenco@outlook.com>
2022-05-03 15:33:15 -0300

    [FEAT] requisito 82

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,11 +3,33 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  /* const { email } = JSON.parse(localStorage.getItem('userInfo.email')); */
   return (
     <>
       <Header title="Profile" searchOn={ false } />
       a
       <Footer />
+      <main>
+        <p data-testid="profile-email">email</p>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+        >
+          Logout
+        </button>
+      </main>
     </>
   );
 }
