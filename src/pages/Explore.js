696650d0adba093538db2d8487aa67ae4611e356
Thamires Lourenço <thami.lourenco@outlook.com>
2022-05-03 16:55:39 -0300

    [feat] Tela de explorar/ requisitos 67 á 69

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Explore() {
+  const history = useHistory();
+
+  const handleClickExploreFoods = () => {
+    history.push('/explore/foods');
+  };
+
+  const handleClickExploreDrinks = () => {
+    history.push('/explore/drinks');
+  };
+
   return (
     <>
       <Header title="Explore" searchOn={ false } />
+      <main>
+        <button
+          data-testid="explore-foods"
+          type="button"
+          onClick={ handleClickExploreFoods }
+        >
+          Explore Foods
+        </button>
+
+        <button
+          data-testid="explore-drinks"
+          type="button"
+          onClick={ handleClickExploreDrinks }
+        >
+          Explore Drinks
+        </button>
+      </main>
       <Footer />
     </>
   );
