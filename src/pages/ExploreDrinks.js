361eb4e4c6bd317d76e797e54f2f21e5010743d0
Luis Borba <luisfpb@icloud.com>
2022-05-03 17:31:39 -0300

    [FEAT] Requisitos 70~73

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExploreDrinks() {
+  const history = useHistory();
+
+  const handleClickExploreIngredient = () => {
+    history.push('/explore/drinks/ingredients');
+  };
+
+  const handleClickExploreNationalities = () => {
+    history.push('/explore/drinks/nationalities');
+  };
+
   return (
     <>
       <Header title="Explore Drinks" searchOn={ false } />
+      <main>
+        <button
+          data-testid="explore-by-ingredient"
+          type="button"
+          onClick={ handleClickExploreIngredient }
+        >
+          By Ingredients
+        </button>
+
+        <button
+          data-testid="explore-by-nationalit"
+          type="button"
+          onClick={ handleClickExploreNationalities }
+        >
+          By Nationality
+        </button>
+
+        <button
+          data-testid="explore-surprise"
+          type="button"
+        >
+          Surprise me!
+        </button>
+      </main>
       <Footer />
     </>
   );
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExploreFoods() {
+  const history = useHistory();
+
+  const handleClickExploreIngredient = () => {
+    history.push('/explore/foods/ingredients');
+  };
+
+  const handleClickExploreNationalities = () => {
+    history.push('/explore/foods/nationalities');
+  };
+
   return (
     <>
       <Header title="Explore Foods" searchOn={ false } />
+      <main>
+        <button
+          data-testid="explore-by-ingredient"
+          type="button"
+          onClick={ handleClickExploreIngredient }
+        >
+          By Ingredients
+        </button>
+
+        <button
+          data-testid="explore-by-nationality"
+          type="button"
+          onClick={ handleClickExploreNationalities }
+        >
+          By Nationality
+        </button>
+
+        <button
+          data-testid="explore-surprise"
+          type="button"
+        >
+          Surprise me!
+        </button>
+      </main>
       <Footer />
     </>
   );
