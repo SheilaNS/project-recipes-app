052391e1152eb2428a6bb5d1ea2c5347d6438599
Luis Borba <luisfpb@icloud.com>
2022-05-06 01:37:23 -0300

    WIP

--- src/pages/Nationalities.js
+++ src/pages/Nationalities.js
@@ -9,6 +9,8 @@ import AppContext from '../context/AppContext';
 function Nationalities() {
   const { meals } = useContext(AppContext);
   const [nationalityData, setNationalityData] = useState([]);
+  const [nationalityEvent, setNationalityEvent] = useState('');
+  // console.log(meals.filter((e) => (e.strArea === 'Turkish')));
 
   useEffect(() => {
     const fetchNationality = async () => {
@@ -18,27 +20,46 @@ function Nationalities() {
     fetchNationality();
   });
 
+  const handleChange = ({ target: { value } }) => {
+    setNationalityEvent(value);
+  };
+
+  // console.log(Object.values(nationalityEvent));
+
   return (
     <>
       <Header title="Explore Nationalities" searchOn />
-      <select data-testid="explore-by-nationality-dropdown">
-        <option>All</option>
+      <select
+        data-testid="explore-by-nationality-dropdown"
+        onChange={ handleChange }
+      >
+        <option
+          value=""
+          data-testid="All-option"
+        >
+          All
+
+        </option>
         {nationalityData
           .reduce((acc, { strArea }) => {
             if (!acc.includes(strArea)) acc = [...acc, strArea];
             return acc;
           }, [])
           .map((nationality, index) => (
-            <option data-testid={ `${nationality}-option` } key={ index }>
+            <option
+              data-testid={ `${nationality}-option` }
+              key={ index }
+            >
               {nationality}
             </option>))}
       </select>
       {meals.length && (
-        meals.reduce((acc, curr, index) => {
-          const maxCards = 12;
-          if (index < maxCards) acc = [...acc, curr];
-          return acc;
-        }, [])
+        meals.filter((e) => (e.strArea.includes(nationalityEvent)))
+          // .reduce((acc, curr, index) => {
+          //   const maxCards = 12;
+          //   if (index < maxCards) acc = [...acc, curr];
+          //   return acc;
+          // }, [])
           .map(({ strMeal, strMealThumb, idMeal }, index) => (
             <Link
               key={ index }
